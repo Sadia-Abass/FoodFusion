76796d9fcd2da15c3f71c3eeddf8e9429c838bd1
Sadia Abass <sadia.abass@outlook.com>
2025-02-17 19:31:03 +0000

    Add properties and data annotation to menu item entity class model

--- FoodFusion/FoodFusion.Server/Entities/MenuItem.cs
+++ FoodFusion/FoodFusion.Server/Entities/MenuItem.cs
@@ -1,6 +1,24 @@
-﻿namespace FoodFusion.Server.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.Entities
 {
     public class MenuItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public long MenuItemID { get; set; }
+        [Required(ErrorMessage = "Enter menu item name")]
+        [StringLength(150)]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Enter a Price")]
+        [Column(TypeName = "decimal(5, 2)")]
+        public decimal Price { get; set; }
+        public long SubCategoryID { get; set; }
+        public SubCategory? SubCategory { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
