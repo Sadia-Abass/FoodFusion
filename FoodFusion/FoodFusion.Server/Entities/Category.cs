36768c96960b962b09f7da5b99ec964877cfeda6
Sadia Abass <sadia.abass@outlook.com>
2025-02-17 18:48:11 +0000

    Add properties and data anotation for category entoty model class

--- FoodFusion/FoodFusion.Server/Entities/Category.cs
+++ FoodFusion/FoodFusion.Server/Entities/Category.cs
@@ -1,6 +1,20 @@
-﻿namespace FoodFusion.Server.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.Entities
 {
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public long CategoryID { get; set; }
+        [Required(ErrorMessage = "Category name is required")]
+        public string Name { get; set; } = string.Empty;
+        public Cuisine? Cuisine { get; set; }
+        public ICollection<SubCategory> SubCategories { get; set; } = new List<SubCategory>();
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
