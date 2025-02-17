6d759a63b56d3cdffcc4ecf8dcd3d59b8eb7179d
Sadia Abass <sadia.abass@outlook.com>
2025-02-17 18:39:24 +0000

    Add properties and data anotation for the cuisine entiy class

--- FoodFusion/FoodFusion.Server/Entities/Cuisine.cs
+++ FoodFusion/FoodFusion.Server/Entities/Cuisine.cs
@@ -1,6 +1,19 @@
-﻿namespace FoodFusion.Server.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.Entities
 {
     public class Cuisine
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public long CuisineID { get; set; }
+        [Required(ErrorMessage = "Cuisine type is required")]
+        public string Type { get; set; } = string.Empty;
+        public List<Category> Categories { get; set; } = new List<Category>();
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
