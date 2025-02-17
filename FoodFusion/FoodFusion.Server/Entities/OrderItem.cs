ec7c8be32e44c5484a55b708601436f5288b04a4
Sadia Abass <sadia.abass@outlook.com>
2025-02-17 21:20:22 +0000

    Add properties and data annotation for order item entity class model

--- FoodFusion/FoodFusion.Server/Entities/OrderItem.cs
+++ FoodFusion/FoodFusion.Server/Entities/OrderItem.cs
@@ -1,6 +1,19 @@
-﻿namespace FoodFusion.Server.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.Entities
 {
     public class OrderItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long OrderItemID { get; set; }
+        public long OrderID { get; set; }
+        public Order Order { get; set; } = new Order();
+        public long MenuItemID { get; set; }
+        public MenuItem MenuItem { get; set; } = new MenuItem();
+        public int Quantity { get; set; }
+        [Column(TypeName = "decimal(5, 2)")]
+        public decimal PriceAtOrder { get; set; }
     }
 }
