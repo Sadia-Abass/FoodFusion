04572cb4f559c8844aea4cf6f4ac49d59bc8c8a7
Sadia Abass <sadia.abass@outlook.com>
2025-02-17 19:49:48 +0000

    Add properties and data anotation for payment entity class model

--- FoodFusion/FoodFusion.Server/Entities/Payment.cs
+++ FoodFusion/FoodFusion.Server/Entities/Payment.cs
@@ -1,6 +1,23 @@
-﻿namespace FoodFusion.Server.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.Entities
 {
     public class Payment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int PaymentID { get; set; }
+        [Required]
+        [Column(TypeName = ("decimal(5, 2)"))]
+        public decimal Amount { get; set; }
+        [Required(ErrorMessage = "Payment type is required")]
+        public string PaymentType { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime PaymentDate { get; set; }
+        public long OrderID { get; set; }
+        public Order? Order { get; set; }
+
     }
 }
