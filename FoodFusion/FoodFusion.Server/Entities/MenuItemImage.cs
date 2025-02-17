725956c194c92f57fd08c14fdcc5280566cde3d5
Sadia Abass <sadia.abass@outlook.com>
2025-02-17 19:37:15 +0000

    Add properties and data annotation for the menu item image entity class model

--- FoodFusion/FoodFusion.Server/Entities/MenuItemImage.cs
+++ FoodFusion/FoodFusion.Server/Entities/MenuItemImage.cs
@@ -1,6 +1,20 @@
-﻿namespace FoodFusion.Server.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.Entities
 {
     public class MenuItemImage
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public long MenuItemImageID { get; set; }
+        [Required]
+        public string ImageUrl { get; set; } = string.Empty;
+        public long MenuItemID { get; set; }
+        public MenuItem? MenuItem { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
