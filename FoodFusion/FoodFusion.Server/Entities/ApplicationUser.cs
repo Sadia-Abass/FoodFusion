55bcd0ca997a310707ecaaaa29fb6257457da168
Sadia Abass <sadia.abass@outlook.com>
2025-03-20 13:00:32 +0000

    Add a custome application user class to extend identity user class.

---
+++ FoodFusion/FoodFusion.Server/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FoodFusion.Server.Entities
+{
+    public class ApplicationUser : IdentityUser 
+    {
+        public string UserType { get; set; } = string.Empty;
+
+        public virtual Customer Customer { get; set; } = new Customer();
+        public virtual Employee Employee { get; set; } = new Employee();
+    }
+}
