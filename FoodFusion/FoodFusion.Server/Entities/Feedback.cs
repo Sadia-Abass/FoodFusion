0c0f9efb7f81c814c48906f32923ad979c87d0b3
Sadia Abass <sadia.abass@outlook.com>
2025-06-12 15:39:33 +0100

    Add properties to feedback entity model.

--- FoodFusion/FoodFusion.Server/Entities/Feedback.cs
+++ FoodFusion/FoodFusion.Server/Entities/Feedback.cs
@@ -1,6 +1,25 @@
-﻿namespace FoodFusion.Server.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.Entities
 {
     public class Feedback
     {
+        public long FeedbackId { get; set; }
+        [Required]
+        public long CustomerId { get; set; }
+        [ForeignKey(nameof(CustomerId))]
+        public Customer Customer { get; set; } = new Customer();
+        [Required]
+        public long MenuItemId { get; set; }
+        [ForeignKey(nameof(MenuItemId))]
+        public MenuItem MenuItem { get; set; } = new MenuItem();
+        [Required]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
+        public int Rating { get; set; }
+        [StringLength(1000, ErrorMessage = "Comment cannot exceed 1000 characters.")]
+        public string Comment { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
