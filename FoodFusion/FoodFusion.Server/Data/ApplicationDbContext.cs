dcf735b5135cde6cbe47bae751fa5385949126b1
Sadia Abass <sadia.abass@outlook.com>
2025-06-23 14:47:25 +0100

    Add application db context

---
+++ FoodFusion/FoodFusion.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,218 @@
+﻿using FoodFusion.Server.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FoodFusion.Server.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string, ApplicationUserClaim, ApplicationUserRole, ApplicationUserLogin,
+        ApplicationRoleClaim, ApplicationUserToken>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {           
+        }
+
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Cancellation> Cancellation { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<CartItem> CartItem { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Cuisine> Cuisine { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Employee> Employee { get; set; }   
+        public DbSet<Feedback> Feedback { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<MenuItemImage> MenuItemImage { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+        public DbSet<Refund> Refund { get; set; }
+        public DbSet<Reservation> Reservation { get; set; }
+        public DbSet<Restaurant> Restaurant { get; set; }
+        public DbSet<Status> Status { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
+
+        // Configuring model properties and relationships
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.HasDefaultSchema("dbo");
+
+            builder.Entity<ApplicationUser>(e =>
+            {
+                // Each User can have many UserClaims
+                e.HasMany(e => e.Claims)
+                    .WithOne(e => e.User)
+                    .HasForeignKey(uc => uc.UserId)
+                    .IsRequired();
+
+                // Each User can have many UserLogins
+                e.HasMany(e => e.Logins)
+                    .WithOne(e => e.User)
+                    .HasForeignKey(ul => ul.UserId)
+                    .IsRequired();
+
+                // Each User can have many UserTokens
+                e.HasMany(e => e.Tokens)
+                    .WithOne(e => e.User)
+                    .HasForeignKey(ut => ut.UserId)
+                    .IsRequired();
+
+                // Each User can have many entries in the UserRole join table
+                e.HasMany(e => e.UserRoles)
+                    .WithOne(e => e.User)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+
+                // 
+                e.ToTable(name: "Users");
+            });
+
+            builder.Entity<ApplicationRole>(e =>
+            {
+                // Each Role can have many entries in the UserRole join table
+                e.HasMany(e => e.UserRoles)
+                    .WithOne(e => e.Role)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                // Each Role can have many associated RoleClaims
+                e.HasMany(e => e.RoleClaims)
+                    .WithOne(e => e.Role)
+                    .HasForeignKey(rc => rc.RoleId)
+                    .IsRequired();
+
+                e.ToTable(name: "Roles");
+            });
+
+            builder.Entity<ApplicationUserClaim>(e =>
+            {
+                e.ToTable(name: "UserClaims");
+            });
+
+            builder.Entity<ApplicationUserLogin>(e =>
+            {
+                e.ToTable(name: "UserLogins");
+            });
+
+            builder.Entity<ApplicationRoleClaim>(e =>
+            {
+                e.ToTable(name: "RoleClaims");
+            });
+
+            builder.Entity<ApplicationUserToken>(e =>
+            {
+                e.ToTable(name: "UserTokens");
+            });
+
+            builder.Entity<ApplicationUserRole>(e =>
+            {
+                e.ToTable(name: "UserRoles");
+            });
+
+            // Configure one-to-one relationship for Customer with ApplicationUser
+            builder.Entity<ApplicationUser>()
+                .HasOne(au => au.Customer)
+                .WithOne(c => c.ApplicationUser)
+                .HasForeignKey<Customer>(c => c.ApplicationUserId);
+
+            // Configure one-to-one relationship for Employee with ApplicationUser
+            builder.Entity<ApplicationUser>()
+                .HasOne(au => au.Employee)
+                .WithOne(e => e.ApplicationUser)
+                .HasForeignKey<Employee>(e => e.ApplicationUserId);
+
+            // Configure one-to-one relationship for MenuItem with MenuItemImage
+            builder.Entity<MenuItemImage>()
+                .HasOne(m => m.MenuItem)
+                .WithOne(i => i.MenuItemImage)
+                .HasForeignKey<MenuItem>(i => i.MenuItemID);
+
+            // Configure one-to-many relationship for Cuisine with Category
+            builder.Entity<Cuisine>()
+                .HasMany(c => c.Categories)
+                .WithOne(c => c.Cuisine)
+                .HasForeignKey(c => c.CategoryID);
+
+            // Configure one-to-many relationship for Category with Subcategory
+            builder.Entity<Category>()
+                .HasMany(c => c.Subcategory)
+                .WithOne(s => s.Category)
+                .HasForeignKey(s => s.CategoryID);
+
+            // Configure one-to-many relationship for Subcategory with MenuItem
+            builder.Entity<SubCategory>()
+                .HasMany(s => s.MenuItem)
+                .WithOne(m => m.SubCategory)
+                .HasForeignKey(m => m.SubCategoryID);
+
+            // Configure one-to-many relationship for Order with BillingAddress 
+            builder.Entity<Order>()
+                .HasOne(o => o.BillingAddress)
+                .WithMany()
+                .HasForeignKey(o => o.BillingAddressID);
+
+            // Configure one-to-many relationship for Order with ShippingAddress 
+            builder.Entity<Order>()
+                .HasOne(o => o.ShippingAddress)
+                .WithMany()
+                .HasForeignKey(o =>o.ShippingAddressID);
+
+            // Configure one-to-many relationship for Order with Cancellation
+            builder.Entity<Order>()
+                .HasOne(o => o.Cancellation)
+                .WithOne(c => c.Order)
+                .HasForeignKey<Cancellation>(c => c.OrderId);
+
+            // Configure many-to-one relationship for Payment with Refund
+            builder.Entity<Payment>()
+                .HasOne(p => p.Refund)
+                .WithOne(r => r.Payment)
+                .HasForeignKey<Refund>(r => r.PaymentId);
+
+            // Configure many-to-one relationship for Feedback with Customer
+            builder.Entity<Feedback>()
+                .HasOne(f => f.Customer)
+                .WithMany(c => c.Feedbacks)
+                .HasForeignKey(c => c.CustomerId);
+
+            // Configure many-to-one relationship for Feedback with MenuItem
+            builder.Entity<Feedback>()
+                .HasOne(f => f.MenuItem)
+                .WithMany(p => p.Feedbacks)
+                .HasForeignKey(f => f.MenuItemId);
+
+            // Configure one-to-many relationship for Restaurant with Employee
+            builder.Entity<Restaurant>()
+                .HasMany(r => r.Employee)
+                .WithOne(e => e.Restaurant)
+                .HasForeignKey(e =>  e.RestaurantId);
+
+            // Configure one-to-many relationship for Restaurant with Reservation
+            builder.Entity<Restaurant>()
+                .HasMany(r => r.Reservation)
+                .WithOne(r => r.Restaurant)
+                .HasForeignKey(r => r.ReservationID);
+
+            // Configure one-to-many relationship for Resaurant with MenuItem
+            builder.Entity<Restaurant>()
+                .HasMany(r => r.MenuItem)
+                .WithOne(m => m.Restaurant)
+                .HasForeignKey(m => m.RestaurantId);
+
+            // Configure one-to-many relationship for Reservation with Customer
+            builder.Entity<Reservation>()
+                .HasOne(r => r.Customer)
+                .WithMany(c => c.Reservation)
+                .HasForeignKey(c => c.ReservationID);
+
+            // Configure one-to-many relationship for Restaurant with Cuisine
+            builder.Entity<Restaurant>()
+                .HasMany(r => r.Cuisine)
+                .WithOne(c => c.Restaurant)
+                .HasForeignKey(r => r.RestaurantId);
+        }
+    }
+}
