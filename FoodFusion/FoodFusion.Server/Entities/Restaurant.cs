71272de8a8550beb867158a0445f83ca1d34b8d5
Sadia Abass <sadia.abass@outlook.com>
2025-06-30 17:35:51 +0100

    Add logo and numbers of tables property.

--- FoodFusion/FoodFusion.Server/Entities/Restaurant.cs
+++ FoodFusion/FoodFusion.Server/Entities/Restaurant.cs
@@ -8,28 +8,43 @@ namespace FoodFusion.Server.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long RestaurantId { get; set; }
+
         [Required(ErrorMessage = "Restaurant name is required")]
         [StringLength(50)]
         public string Name { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "Address Line 1 is required")]
         [StringLength(150)]
         public string AddressLineOne { get; set; } = string.Empty;
+
         [StringLength(150)]
         public string AddressLineTwo { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "City is required")]
         [StringLength(100)]
         public string City { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "Post code is required")]
         [StringLength(12)]
         public string PostCode { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "Phone number is required")]
         public string PhoneNumber { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "Number of Day of week open is required.")]
         public int DaysOfWeek { get; set; }
+
         [Required(ErrorMessage = "Opening time is required")]
         public TimeSpan OpeningTime {  get; set; }
+
         [Required(ErrorMessage = "Closing time is required")]
         public TimeSpan ClosingTime { get; set; }
+
+        [Required(ErrorMessage = "Numbers of available tables is required.")]
+        public int NumbersOfTables { get; set; }
+
+        public string Logo { get; set; } = string.Empty;
+
         public ICollection<Cuisine> Cuisine { get; set; } = new List<Cuisine>();
         public ICollection<MenuItem> MenuItem { get; set; } = new List<MenuItem>();
         public ICollection<Employee> Employee { get; set; } = new List<Employee>();
