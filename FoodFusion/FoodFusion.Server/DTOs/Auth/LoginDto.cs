86eb861ad4736315416f158bdadc03b762dd2272
Sadia Abass <sadia.abass@outlook.com>
2025-07-14 23:54:55 +0100

    Add login dto

---
+++ FoodFusion/FoodFusion.Server/DTOs/Auth/LoginDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodFusion.Server.DTOs.Auth
+{
+    public class LoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
