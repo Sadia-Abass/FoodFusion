4ab875538b04e8dcf0de8835014d6a0b196a9ed3
Sadia Abass <sadia.abass@outlook.com>
2025-07-15 02:00:19 +0100

    Add registration dto

---
+++ FoodFusion/FoodFusion.Server/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodFusion.Server.DTOs.Auth
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Compare("Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(25, ErrorMessage = "the {0} must be at least {1} characters long.", MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [StringLength(25, ErrorMessage = "the {0} must be at least {1} characters long.",  MinimumLength = 8)]
+        public string ConfirmPassword { get; set; }
+    }
+}
