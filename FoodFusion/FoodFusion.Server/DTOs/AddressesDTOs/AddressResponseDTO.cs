8c1ee325f3eaf07bf1255cbaa7c0ad86464b790b
Sadia Abass <sadia.abass@outlook.com>
2025-09-12 13:56:58 +0100

    Add address response dto.

---
+++ FoodFusion/FoodFusion.Server/DTOs/AddressesDTOs/AddressResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using FoodFusion.Server.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.DTOs.AddressesDTOs
+{
+    public class AddressResponseDTO
+    {
+        public long Id { get; set; }
+        public long CustomerID { get; set; }
+        public string AddressLineOne { get; set; } = string.Empty;
+        public string AddressLineTwo { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string County { get; set; } = string.Empty;
+        public string PostCode { get; set; } = string.Empty;
+    }
+}
