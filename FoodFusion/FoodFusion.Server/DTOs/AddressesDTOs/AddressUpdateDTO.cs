057ac068cf9002f53f3b8ebf4175e1472209cba5
Sadia Abass <sadia.abass@outlook.com>
2025-09-12 14:01:31 +0100

    Add address update dto.

---
+++ FoodFusion/FoodFusion.Server/DTOs/AddressesDTOs/AddressUpdateDTO.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodFusion.Server.DTOs.AddressesDTOs
+{
+    public class AddressUpdateDTO
+    {
+        [Required(ErrorMessage = "AddressId is required.")]
+        public long AddressID { get; set; }
+
+        [Required(ErrorMessage = "CustomerId is required.")]
+        public long CustomerID { get; set; }
+
+        [Required(ErrorMessage = "Address line 1 is required.")]
+        [StringLength(255, ErrorMessage = "Address Line 1 cannot exceed 255 characters.")]
+        public string AddressLineOne { get; set; } = string.Empty;
+
+        [StringLength(255, ErrorMessage = "Address Line 2 cannot exceed 255 characters.")]
+        public string AddressLineTwo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(100, ErrorMessage = "City cannot exceed 100 characters.")]
+        public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "County is required.")]
+        [StringLength(50, ErrorMessage = "County cannot exceed 50 characters.")]
+        public string County { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Postcode is required")]
+        [RegularExpression(@"^[A-Z]{1,2}[0-9R][0-9A-Z]?●[0-9][ABD-HJLNP-UW-Z]{2}$", ErrorMessage = "Invalid Postcode")]
+        public string PostCode { get; set; } = string.Empty;
+    }
+}
