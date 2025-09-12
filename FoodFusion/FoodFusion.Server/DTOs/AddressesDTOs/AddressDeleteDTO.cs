8d8741cfd579a19f8c86a21b995aa0e7e164bec4
Sadia Abass <sadia.abass@outlook.com>
2025-09-12 14:03:03 +0100

    Add address delete dto.

---
+++ FoodFusion/FoodFusion.Server/DTOs/AddressesDTOs/AddressDeleteDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodFusion.Server.DTOs.AddressesDTOs
+{
+    public class AddressDeleteDTO
+    {
+        [Required(ErrorMessage = "AddressId is required.")]
+        public long AddressID { get; set; }
+
+        [Required(ErrorMessage = "CustomerId is required.")]
+        public long CustomerID { get; set; }
+    }
+}
