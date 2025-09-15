07e6b8663f0435e955db5c8cdcdbfaf5f74fad69
Sadia Abass <sadia.abass@outlook.com>
2025-09-15 17:27:05 +0100

    Add customer response dto to handle customer information.

---
+++ FoodFusion/FoodFusion.Server/DTOs/CustomerDTOs/CustomerResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using FoodFusion.Server.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.DTOs.CustomerDTOs
+{
+    public class CustomerResponseDTO
+    {
+        public long CustomerID { get; set; }
+        public string ApplicationUserId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+    }
+}
