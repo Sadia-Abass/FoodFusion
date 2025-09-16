7208ed6cd6520f18885feb42d192f40d02494ee1
Sadia Abass <sadia.abass@outlook.com>
2025-09-16 13:17:06 +0100

    Add order response dto and required properties

---
+++ FoodFusion/FoodFusion.Server/DTOs/OrdersDTOs/OrderResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿using FoodFusion.Server.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.DTOs.OrdersDTOs
+{
+    public class OrderResponseDTO
+    {
+        public long OrderID { get; set; }
+        public string OrderNumber { get; set; } = string.Empty;
+        public DateTime OrderDate { get; set; }
+        public long CustomerID { get; set; }
+        public long BillingAddressID { get; set; }
+        public long ShippingAddressID { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Discount { get; set; }
+        public decimal DeliveryFee { get; set; }
+        public decimal Total { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+    }
+}
