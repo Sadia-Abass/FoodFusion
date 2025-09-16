ae46c81643dfbc84044add2f57bfa6113fa74074
Sadia Abass <sadia.abass@outlook.com>
2025-09-16 13:19:10 +0100

    Add reservation response dto and required properties

---
+++ FoodFusion/FoodFusion.Server/DTOs/ReservationResponseDTOs/ReservationResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿using FoodFusion.Server.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.DTOs.ReservationResponseDTOs
+{
+    public class ReservationResponseDTO
+    {
+        public long ReservationID { get; set; }
+        public long CustomerID { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string CustomerEmail { get; set; } = string.Empty;
+        public string CustomerPhonenumber { get; set; } = string.Empty;
+        public DateTime ReservationDate { get; set; }
+        public DateTime ReservationTime { get; set; }
+        public int NumberOfPeople { get; set; }
+        public string SpecialRequests { get; set; } = string.Empty;
+        public long RestaurantId { get; set; }
+    }
+}
