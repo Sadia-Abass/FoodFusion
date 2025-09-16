8339aee0609dec874290bef2520cf32e226d433c
Sadia Abass <sadia.abass@outlook.com>
2025-09-16 13:19:42 +0100

    Add customer repositoty inferface

---
+++ FoodFusion/FoodFusion.Server/Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using FoodFusion.Server.DTOs;
+using FoodFusion.Server.DTOs.CustomerDTOs;
+using FoodFusion.Server.Entities;
+
+namespace FoodFusion.Server.Repositories.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<ApiResponse<CustomerResponseDTO>> GetCustomerById(long id);
+        Task<ApiResponse<CustomerResponseDTO>> RegisterCustomerAsync(CustomerRegistrationDTO customerRegisterDto);
+        Task<ApiResponse<ConfirmationResponseDTO>> UpdateCustomerAsync(CustomerUpdateDTO customerUpdateDto);
+        Task<ApiResponse<ConfirmationResponseDTO>> DeleteCustomerAsync(long id);
+    }
+}
