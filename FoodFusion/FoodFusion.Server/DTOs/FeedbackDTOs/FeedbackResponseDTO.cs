22751ffbb6330eb7de063639ec82695dfdc4cfed
Sadia Abass <sadia.abass@outlook.com>
2025-09-16 13:16:26 +0100

    Add feedback response dto and properties.

---
+++ FoodFusion/FoodFusion.Server/DTOs/FeedbackDTOs/FeedbackResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using FoodFusion.Server.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodFusion.Server.DTOs.FeedbackDTOs
+{
+    public class FeedbackResponseDTO
+    {
+        public long FeedbackId { get; set; }
+        public long CustomerId { get; set; }
+        public Customer Customer { get; set; } = new Customer();
+        public long RestaurantId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+    }
+}
