ca93148a512d3bdf37daf50f676f8c28b30ac4bf
Sadia Abass <sadia.abass@outlook.com>
2025-09-26 23:55:54 +0100

    Add authentication service interface.

---
+++ FoodFusion/FoodFusion.Server/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using FoodFusion.Server.DTOs;
+using FoodFusion.Server.DTOs.Auth;
+
+namespace FoodFusion.Server.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<ApiResponse<TokenResponseDTO>> RegisterAsync(RegisterDto registerDto);
+        Task<ApiResponse<TokenResponseDTO>> LoginAsync(LoginDto loginDto);
+        Task<ApiResponse<TokenResponseDTO>> RefreshTokenAsync(RefreshTokenDTO refreshTokenDTO);
+        Task<bool> LogoutAsync(string userId);
+        Task<ApplicationUserDTO?> GetUserAsync(string userId);
+        Task<ApiResponse<ConfirmationResponseDTO>> ChangePasswordAsync(string userId, ChangePasswordDTO changePasswordDTO);
+        Task<ApiResponse<ConfirmationResponseDTO>> ForgotPasswordAsync(ForgotPasswordDTO forgotPasswordDTO);
+        Task<ApiResponse<ConfirmationResponseDTO>> ResetPasswordAsync(ResetPasswordDTO resetPasswordDTO);
+    }
+}
