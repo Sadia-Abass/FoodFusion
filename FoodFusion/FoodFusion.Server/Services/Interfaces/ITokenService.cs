6f7915cce2206a5f00734372c37aa0c51469bbd5
Sadia Abass <sadia.abass@outlook.com>
2025-09-26 19:40:25 +0100

    Add token service interface.

---
+++ FoodFusion/FoodFusion.Server/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using FoodFusion.Server.Entities;
+using System.Security.Claims;
+
+namespace FoodFusion.Server.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateAccessTokenAsync(ApplicationUser applicationUser);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiryToken(string token);
+        Task<bool> ValidateRefreshTokenAsync(string refershToken, string userId);
+        Task RevokeRefreshTokenAsync(string userId);
+        Task RevokeAllRefreshTokenAsync(string userId);
+    }
+}
