b35e1caf01cc096be921ce962813c1f10b5708ff
Sadia Abass <sadia.abass@outlook.com>
2025-12-02 16:37:30 +0000

    Add interface for restaurant repository.

---
+++ FoodFusion/FoodFusion.Server/Repositories/Interfaces/IRestaurantRepository.cs
@@ -0,0 +1,14 @@
+﻿using FoodFusion.Server.DTOs;
+using FoodFusion.Server.DTOs.RestaurantDTO;
+
+namespace FoodFusion.Server.Repositories.Interfaces
+{
+    public interface IRestaurantRepository
+    {
+        Task<ApiResponse<List<RestaurantResponseDTO>>> GetAllRestarantAsync();
+        Task<ApiResponse<RestaurantResponseDTO>> GetRestaurantByIdAsync(long id);
+        Task<ApiResponse<RestaurantResponseDTO>> CreateRestaurantAsync(CreateRestaurantDTO createRestaurantDTO);
+        Task<ApiResponse<ConfirmationResponseDTO>> UpdateRestaurantAsync(UpdateRestaurantDTO updateRestaurantDTO);
+        Task<ApiResponse<ConfirmationResponseDTO>> DeleteRestaurantAsync(long id);
+    }
+}
