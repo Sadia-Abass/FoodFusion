0b5b865146fe21b9f4f7507ce4e4d9b444269c85
Sadia Abass <sadia.abass@outlook.com>
2025-12-18 15:36:09 +0000

    Add fileuploaderresult class.

---
+++ FoodFusion/FoodFusion.Server/FileUploader/FileUploaderResult.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodFusion.Server.FileUploader
+{
+    public class FileUploaderResult
+    {
+        public string PublicId { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+    }
+}
