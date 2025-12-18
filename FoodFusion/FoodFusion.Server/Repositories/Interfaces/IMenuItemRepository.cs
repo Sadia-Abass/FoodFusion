095e41a0267aef446e98ec1d6ec68c27e6821cf3
Sadia Abass <sadia.abass@outlook.com>
2025-12-18 15:40:29 +0000

    Add menuitem implementation details.

--- FoodFusion/FoodFusion.Server/Repositories/Interfaces/IMenuItemRepository.cs
+++ FoodFusion/FoodFusion.Server/Repositories/Interfaces/IMenuItemRepository.cs
@@ -10,9 +10,9 @@ namespace FoodFusion.Server.Repositories.Interfaces
         Task<ApiResponse<MenuItemResponseDTO>> CreateMenuItemAsync(CreateMenuItemDTO createMenuItemDTO);
         Task<ApiResponse<ConfirmationResponseDTO>> UpdateMenuItemAsync(UpdateMenuItemDTO updateMenuItemDTO);
         Task<ApiResponse<ConfirmationResponseDTO>> DeleteMenuItemAsync(long id);
-        Task<ApiResponse<MenuItemListDTO>> GetMenuItemByCuisineAsync(long cuisineId);
-        Task<ApiResponse<MenuItemListDTO>> GetMenuItemByMealTypeAsync(long mealTypeId);
-        Task<ApiResponse<MenuItemListDTO>> GetMenuItemByMealCourseAsync(long mealCourseId);
-        Task<ApiResponse<MenuItemListDTO>> GetMenuItemByDishTypeAsync(long dishTypeId);
+        Task<ApiResponse<List<MenuItemListDTO>>> GetMenuItemByCuisineAsync(long cuisineId);
+        Task<ApiResponse<List<MenuItemListDTO>>> GetMenuItemByMealTypeAsync(long mealTypeId);
+        Task<ApiResponse<List<MenuItemListDTO>>> GetMenuItemByMealCourseAsync(long mealCourseId);
+        Task<ApiResponse<List<MenuItemListDTO>>> GetMenuItemByDishTypeAsync(long dishTypeId);
     }
 }
