3f39481ec749cbbde2b9bb141d69ef50d51fafc0
Sadia Abass <sadia.abass@outlook.com>
2025-12-18 15:41:35 +0000

    Add cloudinary service interface.

---
+++ FoodFusion/FoodFusion.Server/Services/Interfaces/IFileUploaderService.cs
@@ -0,0 +1,11 @@
+﻿using CloudinaryDotNet.Actions;
+using FoodFusion.Server.FileUploader;
+
+namespace FoodFusion.Server.Services.Interfaces
+{
+    public interface IFileUploaderService
+    {
+        Task<ImageUploadResult> AddFileAsync (IFormFile file);
+        Task<string> DeleteFileAsync (string publicId);
+    }
+}
