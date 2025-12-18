c28adc8ef502a46f4ab746c68aa23eb474888071
Sadia Abass <sadia.abass@outlook.com>
2025-12-18 15:39:35 +0000

    Add menuitemimage repository.

---
+++ FoodFusion/FoodFusion.Server/Repositories/Interfaces/IMenuItemImageRepository.cs
@@ -0,0 +1,14 @@
+﻿using FoodFusion.Server.DTOs;
+using FoodFusion.Server.DTOs.MenuItemImageDTOs;
+
+namespace FoodFusion.Server.Repositories.Interfaces
+{
+    public interface IMenuItemImageRepository
+    {
+        Task<ApiResponse<List<MenuItemImageResponseDTO>>> GetMenuItemImageAsync();
+        Task<ApiResponse<MenuItemImageResponseDTO>> GetMenuItemImageByIdAsnc(long id);
+        Task<ApiResponse<MenuItemImageResponseDTO>> CreateMenuItemImageAsync(CreateMenuItemImageDTO createMenuItemImageDTO);
+        Task<ApiResponse<ConfirmationResponseDTO>> UpdateMenuItemImageAsync(UpdateMenuItemImageDTO updateMenuItemImageDTO);
+        Task<ApiResponse<ConfirmationResponseDTO>> DeleteMenuItemImageAsync(long id);
+    }
+}
